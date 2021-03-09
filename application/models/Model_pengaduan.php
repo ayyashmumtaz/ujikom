15bd9ac0f54ce65ddbd49485c03576fdea52ba60
Ayyash Mumtaz Hafidz <ayyashmumtaz2003@gmail.com>
2021-03-09 08:13:25 +0700

    add carousel / slide function + sample pict

--- application/models/Model_pengaduan.php
+++ application/models/Model_pengaduan.php
@@ -3,7 +3,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Model_pengaduan extends CI_Model {
 
-	
+	public function carousel(){
+          $hasil = $this->db->select('*')
+                            ->get('carousel');
+        if($hasil->num_rows() > 0){
+            return $hasil->result();
+        } else {
+            return array();
+        }
+    
+}
 
 }
 
