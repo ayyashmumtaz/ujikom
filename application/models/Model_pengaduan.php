fe960e702546b5ab884152c279516a5b5578c08b
Ayyash Mumtaz Hafidz <ayyashmumtaz2003@gmail.com>
2021-03-09 07:55:31 +0700

    add Model_pengaduan

---
+++ application/models/Model_pengaduan.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Model_pengaduan extends CI_Model {
+
+	
+
+}
+
+/* End of file Model_pengaduan.php */
+/* Location: ./application/models/Model_pengaduan.php */
