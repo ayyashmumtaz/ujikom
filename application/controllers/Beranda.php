dbedbdc399c7539f96afe0a0cff0c4c41256db08
Ayyash Mumtaz Hafidz <ayyashmumtaz2003@gmail.com>
2021-03-09 08:19:17 +0700

    add construct function

--- application/controllers/Beranda.php
+++ application/controllers/Beranda.php
@@ -3,11 +3,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Beranda extends CI_Controller {
 
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('Model_pengaduan');
+
+	}
+
 	public function index()
 	{
+		$data['banner'] = $this->Model_pengaduan->carousel();
+		
 		$this->load->view('user/_partials/header');
 		$this->load->view('user/_partials/sidebar');
-		$this->load->view('user/index.php');
+		$this->load->view('user/index.php', $data);
 		$this->load->view('user/_partials/footer');
 	}
 
