0ee5886c87e68c347c5775d30d95aacd3d51bbfd
Ayyash Mumtaz Hafidz <ayyashmumtaz2003@gmail.com>
2021-03-09 07:16:05 +0700

    haha i forget to change database config + change beranda controller

---
+++ application/controllers/Beranda.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Beranda extends CI_Controller {
+
+	public function index()
+	{
+		
+	}
+
+}
+
+/* End of file Beranda.php */
+/* Location: ./application/controllers/Beranda.php */
