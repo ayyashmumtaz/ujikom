d75a40d93f051c0e8a61937b61e45a3cf91540fd
Ayyash Mumtaz Hafidz <ayyashmumtaz2003@gmail.com>
2021-03-10 20:40:03 +0700

    form laporan fix with foto

--- application/views/user/form_laporan.php
+++ application/views/user/form_laporan.php
@@ -4,16 +4,16 @@
       <div class="container">
         <!-- Register Form-->
         <div class="register-form mt-5 px-4">
-          <form action="<?= site_url('Laporan/input_data_register');?>" class="form-user" method="post">
+          <form action="<?= site_url('Laporan/input_laporan');?>" class="form-user" method="post" enctype="multipart/form-data">
             <div class="form-group text-left mb-4"><span>ID Pengaduan</span>
               <label for="username"><i class="lni-user"></i></label>
-              <input class="form-control" type="text" name="nik" value="<?= uniqid();?>" readonly>
+              <input class="form-control" type="text" name="id_pengaduan" value="<?= uniqid();?>" readonly>
             </div>
               <div class="form-group text-left mb-4"><span>NIK</span>
               <label for="username"><i class="lni-postcard"></i></label>
-              <input class="form-control" type="text" name="nik" id="form" value="<?= $this->session->userdata('nik');?>" readonly>
+              <input class="form-control" type="text" name="nik" id="form" value="1" readonly>
             </div>
-            <div class="form-group text-left mb-4"><span>Isi Laporan *</span>
+            <div class="form-group text-left mb-4"><span>Isi Laporan</span>
               <label for="username"><i class="lni-library"></i></label>
               <textarea class="form-control" type="text" name="isi_laporan" placeholder="" required=""></textarea>
             </div>
@@ -30,3 +30,15 @@
       </div>
     </div>
 <br>	
+<?php if ($this->session->flashdata('berhasil-input')): ?>
+  <script>
+    Swal.fire({
+      icon: 'success',
+      title: 'Terimakasih telah melaporkan!',
+      text: 'Data Laporan Berhasil Disubmit, silahkan tunggu tanggapan petugas kami dalam 2 x 24 Jam',
+      showConfirmButton: false,
+      timer: 3000
+    })
+  </script>
+
+<?php endif; ?>
