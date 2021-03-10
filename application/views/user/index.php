d7485806853867397024732614c4a77c5642534a
Ayyash Mumtaz Hafidz <ayyashmumtaz2003@gmail.com>
2021-03-10 11:15:33 +0700

    add user dashboard file

---
+++ application/views/user/index.php
@@ -0,0 +1,138 @@
+
+
+<div class="page-content-wrapper">
+  <!-- Hero Slides-->
+
+
+  <div class="hero-slides owl-carousel">
+
+
+  <!-- Single Hero Slide-->
+  <?php foreach($banner as $pro) : ?>
+    <div class="single-hero-slide">
+      <div class="slide-img"><?= img([
+
+        'src'   => 'assets/img/bg-img/' . $pro->img,
+        
+        ])?></div>
+        <div class="slide-content h-100 d-flex align-items-center">
+          <div class="container">
+
+          </div>
+        </div>
+      </div>
+
+    <?php endforeach; ?>
+  </div>
+    </div>
+     
+</div>
+
+
+
+
+  <!-- Product Catagories-->
+  <div class="product-catagories-wrapper" >
+
+
+    <div class="container">
+      <div class="section-heading d-flex align-items-center justify-content-between">
+<h6 class="ml-1">Kategori Layanan Publik</h6>
+          </div>
+      <div class="product-catagory-wrap">
+        <div class="row">
+          <div class="col-4">
+            <div class="card mb-3 catagory-card">
+              <div class="card-body"><a href="<?= site_url('Laporan');?>"><i class="fa fa-warning"></i><span>Laporan</span></a></div>
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="card mb-3 catagory-card">
+              <div class="card-body"><a href="<?= site_url('Laporan/aspirasi');?>"><i class="lni-thought"></i><span>Aspirasi</span></a></div>
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="card mb-3 catagory-card">
+              <div class="card-body"><a href="<?= site_url('Laporan/Informasi');?>"><i class="lni-book"></i><span>Informasi</span></a></div>
+            </div>
+          </div>
+      
+
+
+
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Flash Sale Slide-->
+
+  
+
+
+
+  <br>
+
+ 
+      
+
+     
+
+
+
+
+     
+
+
+
+
+<?php if ($this->session->flashdata('warning')): ?>
+  <script>
+    Swal.fire({
+      icon: 'warning',
+      title: 'Anda Sudah Login!',
+      text: 'Jika tidak dialihkan ke menu utama, silahkan reload halaman ini',
+      showConfirmButton: false,
+      timer: 3000
+    })
+  </script>
+
+<?php endif; ?>
+
+
+
+<?php if ($this->session->flashdata('success-login')): ?>
+  <script>
+    Swal.fire({
+      icon: 'success',
+      title: 'Login Berhasil!',
+      text: 'Selamat Datang di PaduMas!',
+      showConfirmButton: false,
+      timer: 3000
+    })
+  </script>
+
+<?php endif; ?>
+
+
+<?php if ($this->session->flashdata('success-logout')): ?>
+  <script>
+    Swal.fire({
+      icon: 'success',
+      title: 'Logout Berhasil!',
+      text: 'Sampai jumpa! Terimakasih telah mengunjungin PaduMas!',
+      showConfirmButton: false,
+      timer: 3000
+    })
+  </script>
+
+<?php endif; ?>
+<!-- Single Weekly Product Card-->
+
+
+<!-- Top Products-->
+
+
+
+
+<!-- Cool Facts Area-->
+
