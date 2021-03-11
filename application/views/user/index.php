4d33c31c867ad61dcfcedf98b8b1e1d303783eaf
Ayyash Mumtaz Hafidz <ayyashmumtaz2003@gmail.com>
2021-03-11 16:56:26 +0700

    statistic feature

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -1,9 +1,8 @@
 
 
 <div class="page-content-wrapper">
-  <!-- Hero Slides-->
 
-
+  
   <div class="hero-slides owl-carousel">
 
 
@@ -27,17 +26,47 @@
     </div>
      
 </div>
+  <!-- Hero Slides-->
+
+ <div class="product-catagories-wrapper" >
+<br>  
+    <div class="container">
+     
+
+      <div class="section-heading d-flex align-items-center justify-content-between">
+ <div class="col-4">
+            <div class="card mb-3 catagory-card" style="background-color: green;">
+              <div class="card-body"><h3 style="color: white;">286</h3><span style="color: white;">Laporan Telah Selesai</span></div>
+            </div>
+          </div>
+
+          <div class="col-4">
+            <div class="card mb-3 catagory-card" style="background-color: orange;">
+              <div class="card-body"><h3 style="color: white;">30</h3><span style="color: white;">Laporan Diproses</span></div>
+            </div>
+          </div>
+
+          <div class="col-4">
+            <div class="card mb-3 catagory-card" style="background-color: red;">
+              <div class="card-body"><h3 style="color: white;">19</h3><span style="color: white;">Laporan Tidak Diterima</span></div>
+            </div>
+          </div>
+
+
+      </div>
+    </div>
+  </div>
+  
 
 
 
 
-  <!-- Product Catagories-->
+
   <div class="product-catagories-wrapper" >
-
-
+  
     <div class="container">
       <div class="section-heading d-flex align-items-center justify-content-between">
-<h6 class="ml-1">Kategori Layanan Publik</h6>
+<h6 class="ml-1">Kategori Pelayanan Publik</h6>
           </div>
       <div class="product-catagory-wrap">
         <div class="row">
@@ -46,11 +75,13 @@
               <div class="card-body"><a href="<?= site_url('Laporan');?>"><i class="fa fa-warning"></i><span>Laporan</span></a></div>
             </div>
           </div>
+
           <div class="col-4">
             <div class="card mb-3 catagory-card">
               <div class="card-body"><a href="<?= site_url('Laporan/aspirasi');?>"><i class="lni-thought"></i><span>Aspirasi</span></a></div>
             </div>
           </div>
+
           <div class="col-4">
             <div class="card mb-3 catagory-card">
               <div class="card-body"><a href="<?= site_url('Laporan/Informasi');?>"><i class="lni-book"></i><span>Informasi</span></a></div>
@@ -60,14 +91,22 @@
 
 
 
+
+
         </div>
       </div>
     </div>
   </div>
+
+  <!-- Product Catagories-->
+
   <!-- Flash Sale Slide-->
 
-  
 
+    
+
+            
+          
 
 
   <br>
@@ -92,6 +131,19 @@
       title: 'Anda Sudah Login!',
       text: 'Jika tidak dialihkan ke menu utama, silahkan reload halaman ini',
       showConfirmButton: false,
+      timer: 3000
+    })
+  </script>
+
+<?php endif; ?>
+
+<?php if ($this->session->flashdata('not-login')): ?>
+  <script>
+    Swal.fire({
+      icon: 'warning',
+      title: 'Anda Harus Login!',
+      text: 'Anda harus login untuk melakukan pelaporan!',
+      showConfirmButton: true,
       timer: 3000
     })
   </script>
